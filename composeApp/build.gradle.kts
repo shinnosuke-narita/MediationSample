e1fa1278d331becd00a261d15ec46b60a2d7e91a
sin0713 <narisin1124@gmail.com>
2025-09-17 07:50:37 +0900

    Add BGM item

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -42,6 +42,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation("org.jetbrains.compose.material:material-icons-extended:1.7.3")
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
--- composeApp/src/commonMain/kotlin/org/example/mediationsample/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/mediationsample/App.kt
@@ -3,47 +3,84 @@ package org.example.mediationsample
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import mediationsampleproject.composeapp.generated.resources.Res
 import mediationsampleproject.composeapp.generated.resources.compose_multiplatform
+import mediationsampleproject.composeapp.generated.resources.morning
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
         Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+            BGMItem()
         }
     }
+}
+
+@Composable
+fun BGMItem() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth().padding(15.dp)
+    ) {
+        Image(
+            modifier =
+                Modifier
+                    .clip(RoundedCornerShape(20.dp))
+                    .size(90.dp),
+            painter = painterResource(Res.drawable.morning),
+            contentScale = ContentScale.Crop,
+            contentDescription = null
+        )
+
+        Spacer(Modifier.size(40.dp))
+
+        Column {
+            Text(
+                "Summer Feeling",
+                style = MaterialTheme.typography.titleLarge
+            )
+            Spacer(Modifier.size(10.dp))
+            Text("3m15s")
+        }
+
+        Spacer(Modifier.weight(1f))
+        Image(
+            modifier = Modifier.size(40.dp),
+            imageVector = Icons.Default.PlayArrow,
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+            contentDescription = null
+        )
+        Spacer(Modifier.size(15.dp))
+    }
+
 }
